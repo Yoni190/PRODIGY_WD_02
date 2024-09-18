6f5db8c33de032564d3c8f354c4903141313b610
Yoni190 <yfisuhu@gmail.com>
2024-09-18 21:40:47 +0300

    Allow user to stop the watch

--- script.js
+++ script.js
@@ -2,56 +2,69 @@ let screen = document.querySelector(".screen");
 let container = document.querySelector(".container");
 let start = document.getElementById("start");
 let watchInterval = null;
+let started = false;
+
+let millisecond = 1;
+let second = 0;
+let minute = 0;
+
+let msNum = `0${millisecond}`;
+let secNum = `0${second}`;
+let minNum = `0${minute}`;
 
 function startWatch(){
-    let millisecond = 1;
-    let second = 0;
-    let minute = 0;
-
-    let msNum = `0${millisecond}`;
-    let secNum = `0${second}`;
-    let minNum = `0${minute}`;
-    watchInterval = setInterval(()=>{
-        if(millisecond == 100){
-            second++;
-            if(second > 9){
-                secNum = second;
+    started ? started = false : started = true;
+    
+    
+    
+    
+    if (started){
+        start.textContent = "Stop";
+        watchInterval = setInterval(()=>{
+            if(millisecond == 100){
+                second++;
+                if(second > 9){
+                    secNum = second;
+                }
+                else{
+                    secNum = `0${second}`;
+                }
+                
+                millisecond = 0;
             }
-            else{
+            if(second == 60){
+                minute++;
+                if(minute > 9){
+                    minNum = minute;
+                }
+                else{
+                    minNum = `0${minute}`;
+                }
+                second = 0;
+                millisecond = 0;
+                msNum = `0${millisecond}`;
                 secNum = `0${second}`;
             }
-            
-            millisecond = 0;
-        }
-        if(second == 60){
-            minute++;
-            if(minute > 9){
-                minNum = minute;
+            if(millisecond > 9) {
+                msNum = millisecond;
+                screen.textContent = `${minNum}:${secNum}:${msNum}`;
             }
-            else{
-                minNum = `0${minute}`;
+            else {
+                screen.textContent = `${minNum}:${secNum}:${msNum}`;
             }
-            second = 0;
-            millisecond = 0;
-            msNum = `0${millisecond}`;
-            secNum = `0${second}`;
-        }
-        if(millisecond > 9) {
-            msNum = millisecond;
-            screen.textContent = `${minNum}:${secNum}:${msNum}`;
-        }
-        else {
-            screen.textContent = `${minNum}:${secNum}:${msNum}`;
-        }
-        millisecond++;
-    }, 10);
-    start.disabled = true;
+            millisecond++;
+        }, 10);
+
+         }
+    else {
+        clearInterval(watchInterval);
+        start.textContent = "Start";
+    }
 };
 
 function resetWatch(){
     clearInterval(watchInterval);
     screen.textContent = "00:00:00";
-    start.disabled = false;
 }
 
 function lap(){
