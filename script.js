3c7bdeb0ff886c5013e1f7370e9414af6b802fb3
Yoni190 <yfisuhu@gmail.com>
2024-09-18 21:00:26 +0300

    Create a function to start the watch
    I've used the set interval function to increment the value in the screen.

---
+++ script.js
@@ -0,0 +1,46 @@
+let screen = document.querySelector(".screen");
+
+
+function startWatch(){
+    let millisecond = 1;
+    let second = 0;
+    let minute = 0;
+
+    let msNum = `0${millisecond}`;
+    let secNum = `0${second}`;
+    let minNum = `0${minute}`;
+    setInterval(()=>{
+        if(millisecond == 100){
+            second++;
+            if(second > 9){
+                secNum = second;
+            }
+            else{
+                secNum = `0${second}`;
+            }
+            
+            millisecond = 0;
+        }
+        if(second == 60){
+            minute++;
+            if(minute > 9){
+                minNum = minute;
+            }
+            else{
+                minNum = `0${minute}`;
+            }
+            second = 0;
+            millisecond = 0;
+            msNum = `0${millisecond}`;
+            secNum = `0${second}`;
+        }
+        if(millisecond > 9) {
+            msNum = millisecond;
+            screen.textContent = `${minNum}:${secNum}:${msNum}`;
+        }
+        else {
+            screen.textContent = `${minNum}:${secNum}:${msNum}`;
+        }
+        millisecond++;
+    }, 10);
+};
