903c0eea37cff6809f012df922e6a3b748e54603
Yoni190 <yfisuhu@gmail.com>
2024-09-19 08:14:07 +0300

    Add dark mode

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let screen = document.querySelector(".screen");
 let container = document.querySelector(".container");
 let start = document.getElementById("start");
+let title = document.querySelector(".title");
+let buttons = document.querySelectorAll("button");
+let mode = document.querySelector(".mode");
+let laps = document.querySelector(".laps");
 let watchInterval = null;
 let started = false;
+let isDarkMode = false;
 
 let millisecond = 1;
 let second = 0;
@@ -14,9 +19,6 @@ let minNum = `0${minute}`;
 
 function startWatch(){
     started ? started = false : started = true;
-    
-    
-    
     
     if (started){
         start.textContent = "Stop";
@@ -65,11 +67,38 @@ function startWatch(){
 function resetWatch(){
     clearInterval(watchInterval);
     screen.textContent = "00:00:00";
-}
+};
 
 function lap(){
     const para = document.createElement("p");
     const time = document.createTextNode(screen.textContent);
     para.appendChild(time);
-    container.append(para);
+    laps.append(para);
+};
+
+function changeMode() {
+    if (isDarkMode){
+        changeColor("white", "black");
+        mode.innerHTML = "&#127769"
+    }
+    else {
+        changeColor("black", "white");
+        mode.innerHTML = "&#9728"
+    }
+    isDarkMode ? isDarkMode = false : isDarkMode = true;
+    
+};
+
+function changeColor(color1, color2) {
+    document.body.style.background = color1;
+    title.style.color = color2;
+    screen.style.background = color2;
+    screen.style.color = color1;
+    container.style.borderColor = color2;
+
+    laps.style.color = color2;
+    buttons.forEach((button) => {
+        button.style.background = color2;
+        button.style.color = color1;
+    });
 }
